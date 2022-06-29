123355e5f388dda8835f273fd3c4e5dc42e27767
Agustin020 <agustinvidela835@gmail.com>
2022-06-29 14:31:25 -0300

    Cambios

--- gestionst (1).sql
+++ gestionst (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2022 a las 19:30:38
+-- Tiempo de generación: 29-06-2022 a las 19:30:14
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.28
 
@@ -132,6 +132,16 @@ CREATE TABLE `motivos` (
 --
 
 INSERT INTO `motivos` (`id`, `motivos`, `codigoArea`) VALUES
+(2000, 'Cableado nuevo', 2),
+(2001, 'Reparación cableado red', 2),
+(2002, 'Cambio UPS', 2),
+(2003, 'Colocación de antena punto a punto', 2),
+(2004, 'Cambio de antena punto a punto', 2),
+(2005, 'Reparación punto a punto', 2),
+(2006, 'Colocación conectividad wifi', 2),
+(2007, 'Colocación reloj biométrico', 2),
+(2008, 'Retiro de reloj biométrico', 2),
+(2009, 'Crear punto nuevo de conectividad', 2),
 (3000, 'Configuración de impresora', 3),
 (3001, 'Problema de impresora', 3),
 (3002, 'Configuración de PC', 3),
@@ -140,7 +150,31 @@ INSERT INTO `motivos` (`id`, `motivos`, `codigoArea`) VALUES
 (3005, 'Problema con un programa de PC', 3),
 (3006, 'Configuración general del Proxy', 3),
 (3007, 'Configuración de Home Office', 3),
-(6000, 'Turnos', 6);
+(4000, 'Creación de usuario web – SAYGES', 4),
+(4001, 'Creación de usuario agente municipal – SAYGES/GENUS', 4),
+(4002, 'Modificación de permisos – SAYGES', 4),
+(4003, 'Movimientos de expedientes por asignación directa', 4),
+(4004, 'Pruebas por actualización SAYGES', 4),
+(4005, 'Creación de usuario agente municipal - SISTEMA RECLAMOS', 4),
+(4006, 'Modificación de permisos - SISTEMA RECLAMOS', 4),
+(4007, 'Pruebas por actualización - SISTEMA RECLAMOS', 4),
+(4008, 'CONFIGURACIÓN/CREACIÓN – SAYGES', 4),
+(4009, 'CONFIGURACIÓN/CREACIÓN - SISTEMA DE RECLAMOS', 4),
+(4010, 'Creación de Correo Electrónico', 4),
+(4011, 'Elaboración de documentos', 4),
+(4012, 'Capacitación', 4),
+(4013, 'Asistencia remota', 4),
+(4014, 'Investigación', 4),
+(4015, 'Carga de pedidos OPENSAYGES', 4),
+(4016, 'Reuniones', 4),
+(4017, 'Configuración', 4),
+(6000, 'Turnos', 6),
+(7000, 'Colocación de Sistema CCTV', 7),
+(7001, 'Reparación cableado CCTV', 7),
+(7002, 'Colocación de cámara', 7),
+(7003, 'Cambio de cámara', 7),
+(7004, 'Cambio de NVR', 7),
+(7005, 'Grabaciones', 7);
 
 -- --------------------------------------------------------
 
@@ -163,8 +197,20 @@ CREATE TABLE `tareas` (
   `usuario_dni` int(11) DEFAULT NULL,
   `motivoCancelacion` varchar(500) DEFAULT NULL,
   `motivoEliminacion` varchar(500) DEFAULT NULL,
-  `fechaEliminado` datetime DEFAULT NULL
+  `fechaEliminado` datetime DEFAULT NULL,
+  `codigoArea3` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`nroArreglo`, `id_motivos`, `descripcion`, `ip`, `nombreApellidoAfectado`, `celular`, `solucion`, `fechaProblema`, `fechaSolucion`, `estadoTarea_id`, `direccion_codigo`, `usuario_dni`, `motivoCancelacion`, `motivoEliminacion`, `fechaEliminado`, `codigoArea3`) VALUES
+(54, 6000, 'addwazsd', '192.168.0.20', 'awdsfawf', '2612634028', 'adwwdawadwd', '2022-06-27 08:28:52', '2022-06-27 11:57:49', 3, 4005, 2000000, NULL, NULL, NULL, 6),
+(55, 3000, 'klakldaklwdklm', '192.168.70.55', 'Ejemplo Persona', '2612634082', NULL, '2022-06-27 08:32:38', NULL, 1, 4007, 0, NULL, NULL, NULL, 3),
+(56, 3004, 'dvaegwefaafwefaweawf', '192.168.10.80', 'Ejemplo Persona', '2612634096', NULL, '2022-06-27 10:35:03', NULL, 1, 3005, 0, NULL, NULL, NULL, 3),
+(59, 3003, 'adwfasdadwa', '192.168.20.10', '', '', NULL, '2022-06-27 13:28:37', NULL, 1, 3004, 0, NULL, NULL, NULL, 3),
+(64, 2001, 'awdawdweaf', '', 'Ejemplo', '2612634090', NULL, '2022-06-29 13:13:06', NULL, 1, 3004, 0, NULL, NULL, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -215,6 +261,7 @@ INSERT INTO `usuario` (`dni`, `nombre`, `apellido`, `correo`, `usuario`, `contra
 (2000000, 'Agente', 'Ag', 'alguien@gmail.com', 'agente', '$2y$10$fTydZAorPh.XWBjLmQWQUOdC2XeHJw/xhsYM.cX6FynDxxKP4qNtK', 2, NULL, 3),
 (3000000, 'Admin', 'AD', 'adminst@correo.com', 'admin', '$2y$10$aLqdvXR8sYLUsXwH/B/NqeFcWUCFTaUVQ6sPuSWFksxJ/0i36vBoK', 3, NULL, NULL),
 (4000000, 'Supervisor', 'SU', 'alguien@gmail.com', 'supervisor', '$2y$10$T7wcE1CIiWG/FzB935dV3.fTmE4czGoyJiayAHe.SzvAHGNbw/3Ri', 4, NULL, 3),
+(12345667, 'Agente', 'Infraestructura', 'alguien@gmail.com', 'agenteinfr', '$2y$10$z60iSgNERwmQzu1TLnigWOpF9S5srVwH1Ga5.N6kRU59TTAvnYnbm', 2, NULL, 2),
 (42913695, 'Agustin', 'Videla', 'agustinvidela835@gmail.com', 'agustinvidela', '$2y$10$1tmOsQbiLA.DjY0OSYCEN.cqWcd0x0zdf8OsGPrLSoF.p9xe5NyK.', 3, NULL, NULL);
 
 --
@@ -254,7 +301,8 @@ ALTER TABLE `tareas`
   ADD KEY `fk_usuario_dni` (`usuario_dni`),
   ADD KEY `fk_tareas_estadoTarea1_idx` (`estadoTarea_id`),
   ADD KEY `fk_id_motivos` (`id_motivos`),
-  ADD KEY `fk_tareas_area1_idx` (`direccion_codigo`);
+  ADD KEY `fk_tareas_area1_idx` (`direccion_codigo`),
+  ADD KEY `fk_codigoArea3` (`codigoArea3`);
 
 --
 -- Indices de la tabla `tipousuario`
@@ -284,13 +332,13 @@ ALTER TABLE `areas`
 -- AUTO_INCREMENT de la tabla `motivos`
 --
 ALTER TABLE `motivos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6001;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7007;
 
 --
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `nroArreglo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `nroArreglo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- Restricciones para tablas volcadas
@@ -306,6 +354,7 @@ ALTER TABLE `motivos`
 -- Filtros para la tabla `tareas`
 --
 ALTER TABLE `tareas`
+  ADD CONSTRAINT `fk_codigoArea3` FOREIGN KEY (`codigoArea3`) REFERENCES `areas` (`codigo`),
   ADD CONSTRAINT `fk_id_motivos` FOREIGN KEY (`id_motivos`) REFERENCES `motivos` (`id`),
   ADD CONSTRAINT `fk_tareas_direccion` FOREIGN KEY (`direccion_codigo`) REFERENCES `direcciones` (`codigo`),
   ADD CONSTRAINT `fk_tareas_estadoTarea1` FOREIGN KEY (`estadoTarea_id`) REFERENCES `estadotarea` (`id`),
